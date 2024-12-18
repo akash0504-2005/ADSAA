2f799d8402b8a641eaeac62ad68d7b7fa39fef60
akash0504-2005 <sudheerakashkalam@gmail.com>
2024-12-18 15:58:04 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -18,7 +18,7 @@ int n = A.length;
 for (int i = 0; i < n - 1; i++)
 for (int j = 0; j < n - i - 1; j++)
 if (A[j] > A[j + 1]) {
-// Swap A[j] and A[j + 1]
+
 int temp = A[j];
 A[j] = A[j + 1];
 A[j + 1] = temp;
