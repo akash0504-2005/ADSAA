a1c3d72c3200b806ede389b8155213442e6a5419
akash0504-2005 <sudheerakashkalam@gmail.com>
2024-12-20 23:53:51 +0530

    Update insertionsort.java

--- insertionsort.java
+++ insertionsort.java
@@ -1,33 +1,50 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InsertionSort {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of elements: ");
-        int n = sc.nextInt();
-        int[] A = new int[n];
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) 
-            A[i] = sc.nextInt();
 
-        InsertionSort(A);
-
-        System.out.println("Sorted array:");
-        for (int i = 0; i < A.length; i++) 
-            System.out.println(A[i] + " ");
+    
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1; 
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
 
-    static void InsertionSort(int[] A) {
-        int n = A.length;
-        for (int i = 0; i < n ; i++) {
-           
-            int z = A[i];
-            int j=i-1;
-              while(j>=0&&A[j]>z){
-              		A[j+1]=A[j];
-              		j--;
-              		}
-              		 A[j+1]=z;
-         }     		 
-    } 
-} 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Original array: " + Arrays.toString(arr));
+        long startTime = System.nanoTime();
+        insertionSort(arr);
+        long endTime = System.nanoTime();
+        System.out.println("Sorted array: " + Arrays.toString(arr)); 
+        long duration = endTime - startTime;
+        System.out.println("Insertion Sort Time: " + duration + " nanoseconds");
+
+        scanner.close();
+    }
+}
+
+//output
+Enter the number of elements: 6
+Enter the elements:
+12 11 13 5 6
+Original array: [12, 11, 13, 5, 6]
+Sorted array: [5, 6, 11, 12, 13]
+Insertion Sort Time: 500000 nanoseconds
+
+
